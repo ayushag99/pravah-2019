609eb1a641adb86423ce498caff430a89478e961
ayushag99 <37971983+ayushag99@users.noreply.github.com>
2019-09-19 16:44:05 +0530

    Update securities.js

--- setup/securities.js
+++ setup/securities.js
@@ -1,11 +1,3 @@
-// module.exports={
-//     password:"B7CjjGCqbWkZNAE",
-//     username:"ayush",
-//     dburl:"mongodb://ayush:B7CjjGCqbWkZNAE@ds261644.mlab.com:61644/pravah2019",
-//     captchakey:"6LdwUo0UAAAAANtFKATQWN0yNfC0lTZRu2x-3L7i",
-// }
-
-
 module.exports={
     password:process.env.PASSWD,
     username:process.env.USERNAME,
